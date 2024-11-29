d05ece6e071ad27fb027790d6257c4a825bf9578
Rashmeemayee Mohapatra <46987673+rashmi-fit@users.noreply.github.com>
2024-11-29 17:27:58 +0000

    Day1.sql

--- Day1.sql
+++ Day1.sql
@@ -1,4 +1,6 @@
 -- https://leetcode.com/problems/product-sales-analysis-i/?envType=study-plan-v2&envId=top-sql-50
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
 with Sales as (
     select * from Sales
 ),
